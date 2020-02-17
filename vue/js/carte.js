9a72cf58be5b9557ffd927390e1c20ddf98f36cd
mallory94 <malloga@hotmail.fr>
2020-02-17 16:19:52 +0100

    Ajout du token + agrandissement de la carte

--- vue/js/carte.js
+++ vue/js/carte.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('mapid').setView([48.8534, 2.3488], 12);
 
 
 
@@ -8,5 +8,5 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
+    accessToken: 'pk.eyJ1Ijoic2FsdXRhbXlhbWlnbyIsImEiOiJjazZxa3hhMXcwYWFyM25xbGJvOXhtYnp3In0.3CfXpHCip_7Qj8BPrG5FVQ'
 }).addTo(mymap);
